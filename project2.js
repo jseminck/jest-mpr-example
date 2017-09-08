7f22cff0d6473d71c8e2e81c757c04154a2cfcbb
Joachim Seminck <joachim.seminck@zeroturnaround.com>
2017-09-08 19:24:56 +0300

    Reproduce issue

---
+++ project2.js
@@ -0,0 +1,1 @@
+module.exports = {rootDir: './', displayName: 'FRONTEND'};
--- projectB/__tests__/subtract.test.js
+++
@@ -1,4 +0,0 @@
-test('subtracts 3 - 2 to equal 1', () => {
-  const subtract = require('../subtract');
-  expect(subtract(3, 2)).toBe(1);
-});
